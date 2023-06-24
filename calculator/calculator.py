51de6cdb468fd58427e9268b75f3172e9443a42d
Samuel <ogboyesam@gmail.com>
2023-06-24 08:29:54 -0700

    updated

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -29,38 +29,38 @@ def main():
                     result = first_number / second_number
                 elif operand == 5:
                     result = first_number ** second_number
-                elif operand == 6:
-                    print("Select the operation you wish to perform:")
-                    print("1. Square root(√)")
-                    print("2. Logarithm Operations(log)")
-                    print("3. Trigonometry(sin)")
-                    print("4. Trigonometry(cos)")
-                    print("5. Trigonometry(tan)")
-                    advanced_operand = int(
-                        input("Enter the number relating to your choice: "))
-                    if advanced_operand == 1:
-                        result = math.sqrt(first_number)
-                    elif advanced_operand == 2:
-                        result = math.log(first_number)
-                    elif advanced_operand == 3:
-                        result = math.sin(first_number)
-                    elif advanced_operand == 4:
-                        result = math.cos(first_number)
-                    elif advanced_operand == 5:
-                        result = math.tan(first_number)
-                    else:
-                        print("Invalid Operand, please try again")
-                elif operand == 7:
-                    print("Thanks for using this calculator")
-                    exit()
+            elif operand == 6:
+                print("Select the operation you wish to perform:")
+                print("1. Square root(√)")
+                print("2. Logarithm Operations(log)")
+                print("3. Trigonometry(sin)")
+                print("4. Trigonometry(cos)")
+                print("5. Trigonometry(tan)")
+                advanced_operand = int(
+                    input("Enter the number relating to your choice: "))
+                if advanced_operand == 1:
+                    result = math.sqrt(first_number)
+                elif advanced_operand == 2:
+                    result = math.log(first_number)
+                elif advanced_operand == 3:
+                    result = math.sin(first_number)
+                elif advanced_operand == 4:
+                    result = math.cos(first_number)
+                elif advanced_operand == 5:
+                    result = math.tan(first_number)
                 else:
                     print("Invalid Operand, please try again")
-                print("Printing the result...........")
-                print(result)
-                choice = input("You wanna perform more operations? (yes/no): ")
-                if choice.lower() != "yes":
-                    print("Thanks for using this calculator")
-                    break
+            elif operand == 7:
+                print("Thanks for using this calculator")
+                exit()
+            else:
+                print("Invalid Operand, please try again")
+            print("Printing the result...........")
+            print(result)
+            choice = input("You wanna perform more operations? (yes/no): ")
+            if choice.lower() != "yes":
+                print("Thanks for using this calculator")
+                break
         except ValueError:
             print("Hi Sweet, You got value error.")
             print("Make sure you enter only number")
